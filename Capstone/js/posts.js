c00e883b3f8119aa9801c1087b279735a9f81548
Bryan-Thaoxaochay <bthaoxaochay@gmail.com>
2022-03-30 09:35:32 -0500

    Changed how posts are rendered via javascript

--- Capstone/js/posts.js
+++ Capstone/js/posts.js
@@ -1,7 +1,15 @@
-let post = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in...";
+let posts = {
+  Bryan: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in...",
+  David: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in...",
+  Leslie: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in...",
+  George: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in...",
+  Julianne: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in...",
+  Alfred: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in...",
+  Evelyn: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in. Amet risus nullam eget felis eget nunc lobortis. Amet volutpat consequat mauris nunc congue nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Convallis a cras semper auctor neque vitae tempus quam. Morbi tristique senectus et netus et malesuada. Eget est lorem ipsum dolor sit amet. Leo vel orci porta non pulvinar neque laoreet suspendisse. Bibendum ut tristique et egestas quis. Lobortis mattis aliquam faucibus purus in massa. Eget nunc scelerisque viverra mauris in..."
+}
 
-function displayPosts(post) {
-  for (let i = 1; i < 10; i++) {
+function displayPosts(posts) {
+  for (let post in posts) {
     let mainTag = document.getElementById("recentPosts");
     let sectionTag = document.createElement("section");
     let h3Tag = document.createElement("h3");
@@ -11,9 +19,9 @@ function displayPosts(post) {
     sectionTag.appendChild(h3Tag);
     sectionTag.appendChild(pTag);
 
-    h3Tag.innerHTML = "Post " + i;
-    pTag.innerHTML = post;
+    h3Tag.innerHTML = post;
+    pTag.innerHTML = posts[post];
   }
 }
 
-displayPosts(post);
+displayPosts(posts);
